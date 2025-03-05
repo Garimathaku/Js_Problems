eb3829b035fb481cc78e55a0f5ae086653ae0296
Garimathaku <garimathakur397@gmail.com>
2025-03-05 10:41:00 +0530

    [Garima]Add.Day In Range Problem

---
+++ ifelse/DayInRange.js
@@ -0,0 +1,9 @@
+const day = parseInt(process.argv[2]);
+const month = parseInt(process.argv[3]);
+
+const isInRange =
+  (month === 3 && day >= 20) ||
+  (month > 3 && month < 6) ||
+  (month === 6 && day <= 20);
+
+console.log(isInRange);
