a768a0e8f32c19c2cb767acdd73be2a422cfb5ab
Garimathaku <garimathakur397@gmail.com>
2025-03-05 13:42:45 +0530

    [Garima]Add.Prime Number Problem

---
+++ forloop/IsPrimeNumber.js
@@ -0,0 +1,12 @@
+const num = parseInt(process.argv[2]);
+let isPrime = true;
+
+if (num < 2) isPrime = false;
+for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) {
+        isPrime = false;
+        break;
+    }
+}
+
+console.log(`${num} is ${isPrime ? "a Prime" : "not a Prime"} number.`);
